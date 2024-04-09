4eb5c879cef0ace51ba62ed58de3d4ae2f1534c8
nikusha20000 <159459027+nikusha20000@users.noreply.github.com>
2024-04-09 21:10:08 +0400

    added style to JetBrains-copy

---
+++ Jetbrains-copy/index.js
@@ -0,0 +1,5 @@
+let closePopUp = document.querySelector(".close-pop-up");
+
+closePopUp.addEventListener("click", function name(params) {
+    document.getElementById("pop-up").style.display = "none";
+})
