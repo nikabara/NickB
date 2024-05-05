28ad6c0ee1705922c67125a8a229370d945129cb
nikoloz baratashvili <n.baratashvili@itstep.ge>
2024-05-06 01:19:53 +0400

    Added wroking horizontal scrollbar to CakeMenu

--- PopularMenu/index.js
+++ PopularMenu/index.js
@@ -2,29 +2,23 @@ let scrollbar = document.getElementById("scrollbar-top");
 let scrollbar_thumb = document.getElementById("scrollbar-top-thumb");
 
 
-// window.addEventListener("scroll", () => {
-//     const currentScrollHeight = document.documentElement.scrollTop;
-//     var br = window.innerHeight / 15;
-//     var shesh = Math.floor(document.body.getBoundingClientRect().top);
- 
-//     console.log(currentScrollHeight);
-// });
+function updateProgress(num1, num2, item){
+    var percent = Math.ceil( num1 / num2 * 100 ) + '%';
 
-let bodyHeight = document.body.scrollHeight;
-let htmlHeight = document.documentElement.scrollHeight;
-let height = Math.max(bodyHeight, htmlHeight);
-var size = 0;
+    item.style.width = percent;
+    console.log(percent);
 
-document.addEventListener("wheel", (event) => {
-    const delta = event.deltaY || event.detail || event.wheelDelta;
-    const pixelsPerScroll = Math.abs(delta); 
-    size += height / pixelsPerScroll
-    scrollbar_thumb.style.width = `${size}%`;
-    if (size > 100) {
-        size == 100;
+    if(percent == 101 + '%'){
+       item.style.width = "100%";
     }
-    if (size < 0) {
-        size == 0;
+    else if(percent !== 101 + '$'){
+        item.style.width = percent;
     }
-    console.log(size);
+}
+
+window.addEventListener("scroll", () => {
+    var top = window.scrollY;
+    var height = document.body.getBoundingClientRect().height - window.innerHeight;
+
+    updateProgress(top, height, scrollbar_thumb);
 });
