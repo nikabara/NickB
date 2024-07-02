530f0f28661a8c511a3bd9a46bc2892bdd9ecd44
nikoloz baratashvili <n.baratashvili@itstep.ge>
2024-07-03 01:39:16 +0400

    Clickcolor game finished

--- clickcolor/script.js
+++ clickcolor/script.js
@@ -1,7 +1,3 @@
-window.onload = () => {
-
-}
-
 function changeColor () {
     let divColorArray = [];
     for (let i = 1; i <= 8; i++) {
@@ -18,5 +14,36 @@ function changeColor () {
     } 
 
     document.getElementById("change-color").innerText = divColorArray[Math.round(Math.random() * (divColorArray.length - 1))][1]
+    // console.log(document.getElementById("div1").style.backgroundColor);
+    // console.log(document.getElementById("change-color").innerText)
 }
+
+window.onload = () => {
+    changeColor();
+}
+
 document.getElementById("change-color").addEventListener("click", changeColor);
+
+document.querySelectorAll(".color-div").forEach(element => {
+    console.log(element.style.backgroundColor)
+    element.addEventListener("click", () => {
+        if (element.style.backgroundColor == document.getElementById("change-color").innerText) {
+            element.style.border = "solid green 10px"
+            document.querySelector("main").style.pointerEvents = "none";
+            setTimeout(() => {
+                element.style.border = "none"
+                changeColor();
+                document.querySelector("main").style.pointerEvents = "auto";
+            }, 2000);
+        }
+        else {
+            element.style.border = "solid red 10px"
+            document.querySelector("main").style.pointerEvents = "none";
+            setTimeout(() => {
+                element.style.border = "none"
+                changeColor();
+                document.querySelector("main").style.pointerEvents = "auto";
+            }, 500);
+        }
+    })
+});
